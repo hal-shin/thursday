9a83f4e75cd04c439ddf5f40523c085f509d9825
Hal Shin <hal.shin@alumni.ubc.ca>
2022-03-27 12:41:43 -0700

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.+)": "<rootDir>/src/$1",
+  },
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/*.stories.{js,jsx,ts,tsx}",
+    "!src/test/**/*.{js,jsx,ts,tsx}",
+  ],
+};
